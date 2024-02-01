7675e7a6ced1ded3479856f608458757fcce6578
Duc Minh Le <lemduc@gmail.com>
2024-02-01 16:05:23 +0700

    w05 with theme and localisation

--- app/src/main/java/au/edu/swin/sdmd/l04_moreimages/MainActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/l04_moreimages/MainActivity.kt
@@ -1,15 +1,23 @@
 package au.edu.swin.sdmd.l04_moreimages
 
+import android.content.Context
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Switch
+import android.widget.ToggleButton
+import androidx.appcompat.app.AppCompatDelegate
 
 const val KEY_IMAGE = "image_key"
 
 class MainActivity : AppCompatActivity() {
+
+    private var uiMode: Int = Configuration.UI_MODE_NIGHT_NO
+
     override fun onStop() {
         super.onStop()
         Log.i("LIFECYCLE", "stopped")
@@ -68,8 +76,51 @@ class MainActivity : AppCompatActivity() {
 
         val theatre = findViewById<Button>(R.id.theatre)
         theatre.setOnClickListener(onClickTheatre)
+
+        // change theme button
+        val btnTheme = findViewById<ToggleButton>(R.id.theme)
+        btnTheme.apply {
+            textOff = resources.getString(R.string.Light)
+            textOn = resources.getString(R.string.Dark)
+            isChecked  = if (isDarkMode()) true else false
+            setOnClickListener {
+                // important: must use this.context
+                switchDayNightMode(this.context)
+            }
+        }
+
+//        val switchTheme = findViewById<Switch>(R.id.switchTheme)
+//        switchTheme.apply {
+//            isChecked  = if (isDarkMode()) true else false
+//            text  = if (isDarkMode()) "Dark" else "Light"
+//            setOnClickListener {
+//                switchDayNightMode(this.context)
+//            }
+//        }
     }
 
+    /**
+     * @effects
+     *  switch between Day and Night mode on device
+     */
+    private fun switchDayNightMode(context: Context) {
+        val currUIMode = context.resources.configuration.uiMode and
+            Configuration.UI_MODE_NIGHT_MASK
+
+        uiMode = when(currUIMode) {
+            Configuration.UI_MODE_NIGHT_YES ->
+                AppCompatDelegate.MODE_NIGHT_NO
+            else ->
+                AppCompatDelegate.MODE_NIGHT_YES
+        }
+
+        AppCompatDelegate.setDefaultNightMode(uiMode)
+    }
+
+
+    private fun isDarkMode() :Boolean {
+        return uiMode == Configuration.UI_MODE_NIGHT_YES
+    }
 
     /**
      * This is needed to save state. The variables to save need to be
